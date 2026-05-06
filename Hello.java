34ab2bd981d41337c753ffe09672e9db223b6b82
ongodsolomon17 <ongodsolomon17@gmail.com>
2026-05-06 16:51:24 +0100

    Hello Program

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, i am learning Java");
+		System.out.print("oh wow, learning java is fun. ");
+		System.out.print("hurray , i am happy . ");
+		System.out.printf("Java was created in %d%n", 1992);
+	}
+
+}		
